490b7609c5d1b2556cee2cbeae9981d547061d11
ismail-sallami <ismail-sallami@users.noreply.github.com>
2014-03-20 17:53:47 +0100

    Update HttpConnectionApiActivity.java

--- src/com/goeuro/activity/HttpConnectionApiActivity.java
+++ src/com/goeuro/activity/HttpConnectionApiActivity.java
@@ -7,6 +7,7 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Date;
 
 import org.json.JSONArray;
 import org.json.JSONException;
